98ae0825b06cc02af5019a70fdee53fa8f47b7c8
Neeraj Kashyap <nkashy1@gmail.com>
2023-08-05 21:20:29 -0700

    Working implementation of Inventory transformer

--- src/data.ts
+++ src/data.ts
@@ -2,16 +2,46 @@
 // It is consistent with the minimal schema defined in ERC721: https://github.com/ethereum/EIPs/blob/master/EIPS/eip-721.md
 // But it also accommodates fields (like "attributes") which are not defined in the original metadata
 // schema but are commonly used in practice.
-interface ERC721Metadata {
-    name: string;
-    description: string;
-    image: string;
-    attributes?: Attribute[]; // Optional because it's not included in the original schema
+export interface ERC721Metadata {
+  name: string;
+  description: string;
+  image: string;
+  attributes?: Attribute[]; // Optional because it's not included in the original schema
 }
 
 // The Attribute interface is a Typescript representation for metadata attributes as consumed by
 // applications like Open Sea: https://docs.opensea.io/docs/metadata-standards#attributes
-interface Attribute {
-    trait_type: string;
-    value: string | number;
+export interface Attribute {
+  trait_type: string;
+  value: string | number;
+}
+
+// The MetadataTransformer type defines the interface that a metadata transformation must conform to.
+// Note that a MetadataTransformer is expected to be asynchronous by default - even if you are writing
+// a MetadataTransformer that only performs synchronous operations, you should still define it using the
+// async keyword (or promisify or perform an equivalent operation).
+export type MetadataTransformer = (
+  contractAddress: string,
+  tokenID: string,
+  baseMetadata?: ERC721Metadata
+) => Promise<ERC721Metadata>;
+
+export function chain(
+  ...transformers: MetadataTransformer[]
+): MetadataTransformer {
+  return async function (
+    contractAddress: string,
+    tokenID: string,
+    baseMetadata?: ERC721Metadata
+  ): Promise<ERC721Metadata> {
+    let result: ERC721Metadata = baseMetadata || {
+      name: "",
+      description: "",
+      image: "",
+    };
+    for (const transformer of transformers) {
+      result = await transformer(contractAddress, tokenID, result);
+    }
+    return result;
+  };
 }
--- src/server.ts
+++ src/server.ts
@@ -3,31 +3,64 @@
 import cors from "cors";
 import express, { Request, Response } from "express";
 
+import { chain, MetadataTransformer } from "./data";
 import { PORT, web3 } from "./settings";
+import tokenURI from "./transformers/tokenURI";
+import createInventoryTransformer from "./transformers/inventory";
 
-const app = express();
-app.use(cors());
-app.use(express.json());
+export default function run(transformer: MetadataTransformer) {
+  const app = express();
+  app.use(cors());
+  app.use(express.json());
 
-app.get("/status", async (_: Request, res: Response) => {
-  const serverTimestamp = Math.floor(Date.now() / 1000);
-  const chainIDRaw = await web3.eth.getChainId();
-  const chainID = Number(chainIDRaw);
-  const block = await web3.eth.getBlock();
-  const blockNumber = Number(block.number);
-  const blockTimestamp = Number(block.timestamp);
-  const responseBody = {
-    PORT,
-    chainID,
-    blockNumber,
-    blockTimestamp,
-    serverTimestamp,
-  };
-  return res.status(200).json(responseBody);
-});
+  app.get("/status", async (_: Request, res: Response) => {
+    const serverTimestamp = Math.floor(Date.now() / 1000);
+    const chainIDRaw = await web3.eth.getChainId();
+    const chainID = Number(chainIDRaw);
+    const block = await web3.eth.getBlock();
+    const blockNumber = Number(block.number);
+    const blockTimestamp = Number(block.timestamp);
+    const responseBody = {
+      PORT,
+      chainID,
+      blockNumber,
+      blockTimestamp,
+      serverTimestamp,
+    };
+    return res.status(200).json(responseBody);
+  });
 
-app.listen(PORT, () => {
-  console.log(`Metadata Transformer running on ${PORT}`);
-});
+  app.get(
+    "/raw/:contractAddress/:tokenID",
+    async (req: Request, res: Response) => {
+      const contractAddress = req.params.contractAddress;
+      const tokenID = req.params.tokenID;
+      const metadata = await tokenURI(contractAddress, tokenID);
+      return res.status(200).json(metadata);
+    }
+  );
 
-export default app;
+  app.get(
+    "/transformed/:contractAddress/:tokenID",
+    async (req: Request, res: Response) => {
+      const contractAddress = req.params.contractAddress;
+      const tokenID = req.params.tokenID;
+      const metadata = await transformer(contractAddress, tokenID);
+      return res.status(200).json(metadata);
+    }
+  );
+
+  app.listen(PORT, () => {
+    console.log(`Metadata Transformer running on ${PORT}`);
+  });
+}
+
+run(
+  chain(
+    tokenURI,
+    createInventoryTransformer({
+      "0xC740674d2DafF5e59284Fc10a39C862A53BF627D":
+        "0x96f47A4FBBFE506e2EFC60a04E37dE82A8564e8C",
+    })
+  )
+);
