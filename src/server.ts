71d36329c4efd6c5c32c2d79d930d1edb6ddc628
Neeraj Kashyap <nkashy1@gmail.com>
2023-08-07 12:31:00 -0700

    Stopgap error handling

--- src/server.ts
+++ src/server.ts
@@ -6,6 +6,7 @@ import express, { Request, Response } from "express";
 import { chain, MetadataTransformer } from "./data";
 import { PORT, web3 } from "./settings";
 
+// TODO(zomglings): Proper error handling in Express - https://expressjs.com/en/guide/error-handling.html
 export default function run(...transformers: MetadataTransformer[]) {
   if (transformers.length === 0) {
     throw new Error("No transformers provided.");
@@ -19,39 +20,54 @@ export default function run(...transformers: MetadataTransformer[]) {
   app.use(express.json());
 
   app.get("/status", async (_: Request, res: Response) => {
-    const serverTimestamp = Math.floor(Date.now() / 1000);
-    const chainIDRaw = await web3.eth.getChainId();
-    const chainID = Number(chainIDRaw);
-    const block = await web3.eth.getBlock();
-    const blockNumber = Number(block.number);
-    const blockTimestamp = Number(block.timestamp);
-    const responseBody = {
-      PORT,
-      chainID,
-      blockNumber,
-      blockTimestamp,
-      serverTimestamp,
-    };
-    return res.status(200).json(responseBody);
+    try {
+      const serverTimestamp = Math.floor(Date.now() / 1000);
+      const chainIDRaw = await web3.eth.getChainId();
+      const chainID = Number(chainIDRaw);
+      const block = await web3.eth.getBlock();
+      const blockNumber = Number(block.number);
+      const blockTimestamp = Number(block.timestamp);
+      const responseBody = {
+        PORT,
+        chainID,
+        blockNumber,
+        blockTimestamp,
+        serverTimestamp,
+      };
+      return res.status(200).json(responseBody);
+    } catch (e) {
+      console.error(e);
+      return res.status(500).json({ error: e });
+    }
   });
 
   app.get(
     "/raw/:contractAddress/:tokenID",
     async (req: Request, res: Response) => {
-      const contractAddress = req.params.contractAddress;
-      const tokenID = req.params.tokenID;
-      const metadata = await raw(contractAddress, tokenID);
-      return res.status(200).json(metadata);
+      try {
+        const contractAddress = req.params.contractAddress;
+        const tokenID = req.params.tokenID;
+        const metadata = await raw(contractAddress, tokenID);
+        return res.status(200).json(metadata);
+      } catch (e) {
+        console.error(e);
+        return res.status(500).json({ error: e });
+      }
     }
   );
 
   app.get(
     "/transformed/:contractAddress/:tokenID",
     async (req: Request, res: Response) => {
-      const contractAddress = req.params.contractAddress;
-      const tokenID = req.params.tokenID;
-      const metadata = await cumulativeTransform(contractAddress, tokenID);
-      return res.status(200).json(metadata);
+      try {
+        const contractAddress = req.params.contractAddress;
+        const tokenID = req.params.tokenID;
+        const metadata = await cumulativeTransform(contractAddress, tokenID);
+        return res.status(200).json(metadata);
+      } catch (e) {
+        console.error(e);
+        return res.status(500).json({ error: e });
+      }
     }
   );
 
